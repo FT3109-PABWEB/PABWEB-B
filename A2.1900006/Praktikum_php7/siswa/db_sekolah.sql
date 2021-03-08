6f9cb931924e6ec76e2e3a1666da2b10b6aebfe5
agunggumelar00 <a2.1900006@mhs.stmik-sumedang.ac.id>
2021-03-08 15:27:17 +0700

    praktikum PHP Aplikasi Data Siswa

---
+++ A2.1900006/Praktikum_php7/siswa/db_sekolah.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2021 at 09:23 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `nis` varchar(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `tempat_lahir` varchar(30) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') NOT NULL,
+  `agama` varchar(20) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_hp` varchar(13) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`nis`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `alamat`, `no_hp`, `foto`) VALUES
+('19006', 'Agung Gumelar', 'Sumedang', '2001-05-30', 'Laki-laki', 'Islam', 'dusun cikondang desa margamukti kecamatan sumedang utara kabupaten sumedang', '081934178385', 'IMG_20180713_154745.jpg'),
+('19001', 'Abdul Jalil Maulani', 'Sumedang', '2000-01-01', 'Laki-laki', 'Islam', 'cibugel', '081923234545', 'PANO_20180713_154438.jpg'),
+('19002', 'Agung Gunawan', 'Sumedang', '2000-10-30', 'Laki-laki', 'Islam', 'Cikaramas', '081515343421', 'IMG_20180713_154745.jpg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
