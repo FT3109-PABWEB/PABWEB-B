859c20f2d14238445559d111716aba17abbad7ad
AnnisaSucianty <AnnisaSucianty70@gmail.com>
2021-05-05 20:35:35 +0700

    Pengumpulan Praktikum CodeIgniter

---
+++ A2.1900020/Praktikum CodeIgniter/Annisasuc/application/controllers/Halaman_utama.php
@@ -0,0 +1,17 @@
+<?php
+
+class Halaman_utama extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('Login');
+        cek_session();
+    }
+
+    public function index()
+    {
+        $this->load->view('halaman_utama_view');
+    }
+}
