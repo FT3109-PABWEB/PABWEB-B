b28312dccfad76f2726ec08354824e8247bb94a1
albi-s <mohalbis21yaw@gmail.com>
2021-05-10 10:32:31 +0700

    Menambahkan praktikum Codeigniter

---
+++ A2.1900107/Praktikum Codeigniter/application/models/Hello_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+      public function hello_Albi()
+      {
+            return "Hello Albi";
+      }
+
+      public function albi_mvc()
+      {
+            return "ini menggunakan MVC";
+      }
+}
