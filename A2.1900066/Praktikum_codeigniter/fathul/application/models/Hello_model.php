509331f3d3420da5bc0401c83be474cc0c322690
fathulgorib2 <a2.1900066@mhs.stmik-sumedang.ac.id>
2021-06-03 12:44:17 +0700

    Praktikum_Codeigniter

---
+++ A2.1900066/Praktikum_codeigniter/fathul/application/models/Hello_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+
+    public function hello_fathul()
+    {
+        return "Hello Fathul";
+    }
+    public function fathul_mvc()
+    {
+        return "Ini menggunakan MVC";
+    }
+}
