d218bc45e881b3c6d872641997cc0cc849033815
indraanfasa <a2.1900083@mhs.stmik-sumedang.ac.id>
2021-06-03 14:59:28 +0700

    Praktikum CodeIgniter

---
+++ A2.1900083/Praktikum_Codeigniter/mardira/application/views/rowarray_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<head>
+    <title>metode Row Array</title>
+</head>
+
+<h1>Metode Row Array</h1>
+
+<body>
+    <?php echo "nama saya {$rowarray['nama_mhs']} dan NIM saya {$rowarray['NIM']}" ?>
+</body>
+
+</html>
