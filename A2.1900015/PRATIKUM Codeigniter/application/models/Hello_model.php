bf4911cd7ec3a5419c0301b280c34736a4bad728
AldiFebriansyah <mozarella278@gmail.com>
2021-06-05 15:47:13 +0700

    praktikum codeigniter

---
+++ A2.1900015/PRATIKUM Codeigniter/application/models/Hello_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+
+    public function hello_AldiFeb()
+    {
+        return "Hello AnnisaSuc";
+    }
+
+    public function aldifeb_mvc()
+    {
+        return "Ini menggunakan MVC";
+    }
+}
