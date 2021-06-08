e01701131904ae850b976f26323a8268e3e804e9
muhamadrizki13 <a2.1900101@mhs.stmik-sumedang.ac.id>
2021-06-08 14:22:50 +0700

    upload tugas praktikum CI3
    pa ini praktikum CI3

--- A2.1900101/Link.md
+++
@@ -1,3 +0,0 @@
-Silahkan lihat ke
-[Website Saya]( https://webcoding13.000webhostapp.com/   "Websitenya Saya") 
-untuk informasi lebih lengkap
---
+++ A2.1900101/Praktikum codeigniter/rizkiii/application/models/Hello_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+  public function hello_rizkiii()
+  {
+      return " Hello Rizki ";
+  }
+  public function rizkii_mvc()
+  {
+      return " ini menggunakan MVC";
+  }
+}
