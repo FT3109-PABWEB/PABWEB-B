16da6f448fec5c995ad87f72cf24455c80e021bd
nafisaazhar03 <A.1900137@mhs.stmik-sumedang.ac.id>
2021-06-10 17:38:07 +0700

    Upload Praktikum CI3

---
+++ A2.1900137/PAKTIKUM CODEIGNITER/Mardira/application/controllers/Hello.php
@@ -0,0 +1,26 @@
+<?php
+
+class Hello extends CI_Controller
+{
+    public function index()
+    {
+        echo " hello nafisa";
+    }
+    public function hello_model()
+    {
+        $this->load->model('Hello_model');
+        $data = $this -> Hello_model -> hello_nafisa();
+        echo $data;
+    }
+    public function hello_view()
+    {
+        $this->load-> view('hello_view');
+
+    }
+    public function hello_mvc()
+    {
+        $this ->load->model('Hello_model');
+        $data['mvc']=$this->Hello_model->nafisa_mvc();
+        $this->load->view('hello_view',$data);
+    }
+}
---
+++ A2.1900137/PAKTIKUM CODEIGNITER/Mardira/application/models/Hello_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+  public function hello_nafisa()
+  {
+      return " Hello Nafisa ";
+  }
+  public function nafisa_mvc()
+  {
+      return " ini menggunakan MVC";
+  }
+}
