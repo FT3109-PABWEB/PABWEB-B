21e43aec517e0ec81298de115619b7e2a6217349
indraanfasa <a2.1900083@mhs.stmik-sumedang.ac.id>
2021-06-10 20:27:34 +0700

    upload tugas ci4

---
+++ A2.1900083/Praktikum_Codeigniter/mardira/application/views/row_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<head>
+    <title>metode Row</title>
+</head>
+
+<h1>Metode Row</h1>
+
+<body>
+    <?php echo "nama saya {$row->nama_mhs} dan NIM saya {$row->NIM}" ?>
+</body>
+
+</html>
